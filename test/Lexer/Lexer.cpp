2d78a7a12c0d83327ee3ca3357c4bfffff0aceab
GuillaumeGas <guillaume@gas28.net>
2016-11-27 20:44:40 +0100

    maj

--- test/Lexer/Lexer.cpp
+++ test/Lexer/Lexer.cpp
@@ -3,102 +3,107 @@
 using namespace std;
 
 Lexer::Lexer (string file_name) {
-    this->file_name = file_name;
-    this->current_index = 0;
-    this->current_loc.line = 1;
-    this->current_loc.column = 0;
-    this->eof = false;
+  this->file_name = file_name;
+  this->current_index = 0;
+  this->current_loc.line = 0;
+  this->current_loc.column = 0;
+  this->eof = false;
 
-    this->file = new ifstream (file_name.c_str(), std::ios::in);
+  this->file = new ifstream (file_name.c_str(), std::ios::in);
 
-    if (!this->file->is_open()) {
-	cout << "Error file" << endl;
-	throw -1;
-    }
+  if (!this->file->is_open()) {
+    cout << "Error file" << endl;
+    throw -1;
+  }
 }
 
 Lexer::~Lexer () {
-    if (this->file) {
-	this->file->close ();
-	delete this->file;
-    }
+  if (this->file) {
+    this->file->close ();
+    delete this->file;
+  }
 }
 
 bool Lexer::isEof () const {
-    return this->eof;
+  return this->eof;
 }
 
 void Lexer::setKeys (vector<string> keys) {
-    for (auto it : keys) {
-	this->keys.push_back (it);
-    }
+  for (auto it : keys) {
+    this->keys.push_back (it);
+  }
 }
 
 //TODO : si on doit passer des commentaires, on les passe...
 Token Lexer::next () { return this->get_word (); }
 
 Token Lexer::get_word () {
-    string line = this->read_line (this->current_index);
+  string line = this->read_line (this->current_index);
 
-    if (line.size() == 0)
-	return TokenEof (current_loc);
+  if (line.size() == 0)
+    return TokenEof (current_loc);
 
-    int max = 0, pos = line.size();
-    string tok = "";
-    for (auto it : this->keys) {
-	int index = line.find (it);
+  int max = 0, pos = line.size();
+  int index = -1;
+  string tok = "";
+  for (auto it : this->keys) {
+    index = line.find (it);
 
-	if (index > 0) {
-	    if (index < pos) {
-		pos = index;
-		max = it.size();
-		tok = it;
-	    } else if (index == pos) {
-		if (max < it.size()) {
-		    max = it.size();
-		    tok = it;
-		}
-	    }
+    if (index > 0) {
+      if (index < pos) {
+	pos = index;
+	max = it.size();
+	tok = it;
+      } else if (index == pos) {
+	if (max < it.size()) {
+	  max = it.size();
+	  tok = it;
 	}
+      }
     }
+  }
 
-    location_t loc;
-    loc.line = this->current_loc.line;
-    if (pos == -1) {
-	Token t = Token (line, this->current_loc);
-	this->current_index += line.size();
-	return t;
-    } else if (pos == current_pos) {
-	loc.column = current_pos;
-	this->current_index += tok.size();
-	return Token (tok, loc);
-    } else {
-	Token t = Token (line.substr (this->current_loc.column, pos), this->current_loc);
-	this->current_loc.column += (pos - this->current_loc.column);
-    }
+  cout << tok << endl;
+
+  location_t loc;
+  loc.line = this->current_loc.line;
+  loc.column = this->current_loc.column;
+
+  if (index == -1) {
+    this->current_index += line.size();
+    return Token (line, loc);
+  } else if (index == loc.column) {
+    this->current_loc.column += tok.size();
+    this->current_index += tok.size();
+    return Token (tok, loc);
+  } else {
+    this->current_index += (pos - this->current_loc.column);
+    this->current_loc.column += (pos - this->current_loc.column);
+    return Token (line.substr (loc.column, pos), loc);
+  }
 }
 
 string Lexer::read_line (unsigned int offset) {
-    this->file->seekg (offset);
-    string res = "";
+  this->file->seekg (offset);
+  string res = "";
+  getline (*this->file, res);
+  if (res.size() == 0) {
     getline (*this->file, res);
-    if (res.size() == 0) {
-	getline (*this->file, res);
-	this->current_index++;
-	loc.line++;
-	loc.column = 0;
-    }
-    file->seekg (0);
-    return res;
+    this->current_index++;
+    this->current_loc.line++;
+    this->current_loc.column = 0;
+  }
+  file->seekg (0);
+  return res;
 }
 
 Token::Token (string _value, location_t _loc) {
-    value = _value;
-    loc = _loc;
+  value = _value;
+  loc = _loc;
 }
 
 string Token::to_string() const {
-    return "<" + value + ", loc(" + std::to_string(loc.line) + ", " + std::to_string(loc.column) + ">";
+  return "<" + value + ", loc(" + std::to_string(loc.line) + ", " + std::to_string(loc.column) + ")>";
 }
 
 TokenEof::TokenEof (location_t _loc) : Token ("EOF", _loc) {}
